66779a3160903dd056840753a5d17bf9b2c3aa16
Muhsin Azmal <muhsinstudent12@gmail.com>
2025-03-02 20:57:13 +0600

    lzgit - 2025-03-02T14:57:12.511Z

--- resources/js/Pages/Layout.jsx
+++ resources/js/Pages/Layout.jsx
@@ -7,16 +7,15 @@ const navigation = [
     { name: 'টপিক - ৩', href: '/' },
     { name: 'টপিক - ৪', href: '/' },
 ]
-console.log(navigation);
 
 export default function Layout({ children }) {
     return (
         <main>
             <header className='flex gap-4'>
                 {navigation.map((item, index) => (
-                    <Link className='px-4 py-2 bg-gray-200' key={index} href={route('blogs.index')}>
+                    <a className='px-4 py-2 bg-gray-200' key={index} href={item.href}>
                         {item.name}
-                    </Link>
+                    </a>
                 ))}
             </header>
             <article>{children}</article>
